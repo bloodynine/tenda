a49e2a78bea8e3d34bc6826d65c09875f41fb23b
Kerk Wilson <kewilson@factsmgt.com>
2022-01-17 11:24:13 -0600

    Fixed Local

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tenda.kerk.xyz/api'
+  apiUrl: 'https://tenda2.kerk.xyz/api'
 };
--- Program.cs
+++ Program.cs
@@ -76,7 +76,11 @@ app.MapWhen(y => !y.Request.Path.StartsWithSegments("/api"), x =>
     x.UseSpa(spa =>
     {
         // spa.Options.SourcePath = "ClientApp";
-        if (app.Environment.IsDevelopment()) spa.UseAngularCliServer("start");
+        if (app.Environment.IsDevelopment())
+        {
+            spa.Options.SourcePath = "ClientApp";
+            spa.UseAngularCliServer("start");
+        }
     });
 });
 
