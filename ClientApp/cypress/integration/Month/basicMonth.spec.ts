775fc00a8b4ae8dadd96b717140547c84fae4874
Kerk Wilson <kewilson@factsmgt.com>
2022-01-18 10:25:06 -0600

    Test formatting

--- ClientApp/cypress/integration/Month/basicMonth.spec.ts
+++ ClientApp/cypress/integration/Month/basicMonth.spec.ts
@@ -1,26 +1,30 @@
-
+const date = 'Dec 1, 2022';
 
 describe('Month', () => {
   beforeEach(() => {
     cy.apiLogin({});
     cy.goToMonth({year: 2022, month: 12})
   })
-  it('should display', () => {
-    const date = 'Dec 1, 2022'
 
+  it('should display', () => {
     cy.get('.container').children().should('have.length', 31)
     cy.getDay(date).find('#bills')
     cy.getDay(date).find('#incomes')
     cy.getDay(date).find('#oneOffs')
   })
 
+  it('should load a new month', () => {
+    cy.goToMonth({year: 2022, month: 2})
+    cy.get('.container').children().should('have.length', 28)
+  })
+
   it('should add income', () => {
-    cy.dayTotal('Dec 1, 2022').then( (beforeAmount) => {
-      cy.getDay('Dec 1, 2022').find('.divButton').contains('Add Income').click();
+    cy.dayTotal(date).then( (beforeAmount) => {
+      cy.getDay(date).find('.divButton').contains('Add Income').click();
       cy.transactionFill('test1', 7);
-      cy.dayTotal('Dec 1, 2022').should('eq', beforeAmount + 7)
+      cy.dayTotal(date).should('eq', beforeAmount + 7)
     })
-    cy.getDay('Dec 1, 2022')
+    cy.getDay(date)
       .find('#incomes')
       .children()
       .contains('test1');
@@ -29,7 +33,6 @@ describe('Month', () => {
   })
 
   it('should add a bill', () => {
-    const date = 'Dec 1, 2022'
     cy.dayTotal(date).then((beforeAmount) => {
         cy.getDay(date).find('.divButton')
           .contains('Add Bill')
@@ -45,7 +48,6 @@ describe('Month', () => {
   })
 
   it('should add a oneOff', () => {
-    const date = 'Dec 1, 2022'
     cy.dayTotal(date).then((beforeAmount) => {
       cy.getDay(date).find('.divButton')
         .contains('Add Transaction')
