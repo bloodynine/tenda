4e5cec3335b326a706e3d3c4dd55214d0531e67a
Kerk Wilson <kewilson@factsmgt.com>
2022-01-19 14:18:25 -0600

    repeat form validation

--- ClientApp/src/app/repeat-form/repeat-form.component.ts
+++ ClientApp/src/app/repeat-form/repeat-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
 import { RepeatService } from "../repeat.service";
 import { RepeatContract } from "../Shared/Interfaces/RepeatContract";
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { RepeatType, RepeatTypeLabel } from "../Shared/Interfaces/RepeatSettings";
 import { StateService } from "../state.service";
 
@@ -22,16 +22,24 @@ export class RepeatFormComponent implements OnInit {
   ) {
   }
 
+  doesFieldHaveError(fieldName: string, error?: string): boolean {
+    const pristine = this.form.get(fieldName)?.pristine;
+    if(!error){
+      return !pristine && (this.form.get(fieldName)?.invalid ?? false);
+    }
+    return !pristine && (this.form.get(fieldName)?.hasError(error) ?? false);
+  }
 
   ngOnInit(): void {
+    const numRegex = /^-?\d*[.,]?\d{0,2}$/;
     this.stateService.editingRepeatSettings.subscribe(x => {
       this.repeatContract = x
       this.form = new FormGroup({
-        name: new FormControl(x.name),
+        transactionName: new FormControl(x.name, [Validators.required]),
         interval: new FormControl(x.interval),
         repeatFrequency: new FormControl(x.repeatType),
         startDate: new FormControl(x.startDate),
-        amount: new FormControl(x.amount)
+        amount: new FormControl(x.amount, [Validators.required, Validators.pattern(numRegex)])
       });
     });
   }
@@ -41,7 +49,7 @@ export class RepeatFormComponent implements OnInit {
       this.repeatContract.repeatType = parseInt(this.form.get('repeatFrequency')?.value);
       this.repeatContract.interval = this.form.get('interval')?.value;
       this.repeatContract.startDate = new Date(this.form.get('startDate')?.value);
-      this.repeatContract.name = this.form.get('name')?.value;
+      this.repeatContract.name = this.form.get('transactionName')?.value;
       this.repeatContract.amount = this.form.get('amount')?.value;
       this.repeatService.UpdateRepeatContract(this.repeatContract.id, this.repeatContract)
         .then(x => {
